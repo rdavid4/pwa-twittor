e05fb154ef8de537490590ed32cccbe3cf786866
Roger Quiñonez <rogerdavid444@gmail.com>
2019-08-19 20:45:07 +0200

    Cambios sw

--- sw.js
+++ sw.js
@@ -44,9 +44,6 @@ self.addEventListener('activate', e =>{
             if(key !== STATIC_CACHE && key.includes('static')){
                 return caches.delete(key)
             }
-            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
-                return caches.delete(key);
-            }
         })
     })
 
