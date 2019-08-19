06ec17178d8ef8ecc8be41d1aace1ab41449ef56
Roger Quiñonez <rogerdavid444@gmail.com>
2019-08-19 20:13:17 +0200

    Modificacion produccion github

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-importScripts('/js/sw-utils.js')
+importScripts('js/sw-utils.js')
 
 const STATIC_CACHE = 'static-v1'
 const DYNAMIC_CACHE = 'dynamic-v1'
 const INMUTABLE_CACHE = 'inmutable-v1'
 
 const APP_SHELL = [
-    '/',
+    //'/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
